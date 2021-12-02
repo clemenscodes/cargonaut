407dabd08efb0882a223962819c0a307fd489ea0
Clemens Horn <clemens.horn@mni.thm.de>
2021-12-02 01:31:18 +0100

    add firebase client module
    Signed-off-by: Clemens Horn <clemens.horn@mni.thm.de>

--- apps/cargonaut/src/app/app.module.ts
+++ apps/cargonaut/src/app/app.module.ts
@@ -2,10 +2,18 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { HttpClientModule } from '@angular/common/http';
+import { AngularFireModule } from '@angular/fire/compat';
+import { FirestoreModule } from '@angular/fire/firestore';
+import { environment } from '@env';
 
 @NgModule({
     declarations: [AppComponent],
-    imports: [BrowserModule, HttpClientModule],
+    imports: [
+        BrowserModule,
+        HttpClientModule,
+        AngularFireModule.initializeApp(environment.firebase),
+        FirestoreModule,
+    ],
     providers: [],
     bootstrap: [AppComponent],
 })
