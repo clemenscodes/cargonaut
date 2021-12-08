7901aea9c5094ec94c2b0c75bf70b824009c9ef4
Clemens Horn <clemens.horn@mni.thm.de>
2021-12-08 13:24:47 +0100

    fix codesmells
    Signed-off-by: Clemens Horn <clemens.horn@mni.thm.de>

--- tools/scripts/run-many.js
+++ tools/scripts/run-many.js
@@ -1,6 +1,6 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var child_process_1 = require("child_process");
+'use strict';
+Object.defineProperty(exports, '__esModule', { value: true });
+var child_process_1 = require('child_process');
 var target = process.argv[2];
 var jobIndex = Number(process.argv[3]);
 var jobCount = Number(process.argv[4]);
@@ -9,22 +9,38 @@ var baseSha = isMain ? 'main~1' : 'main';
 function restArgs() {
     return process.argv
         .slice(6)
-        .map(function (arg) { return "\"" + arg + "\""; })
+        .map(function (arg) {
+            return '"' + arg + '"';
+        })
         .join(' ');
 }
-var affected = "npx nx print-affected --base=" + baseSha + " --target=" + target;
+var affected =
+    'npx nx print-affected --base=' + baseSha + ' --target=' + target;
 affected = (0, child_process_1.execSync)(affected).toString('utf-8');
 var tasksArray = JSON.parse(affected).tasks.sort();
-var projectsArray = tasksArray.map(function (task) { return task.target.project; });
+var projectsArray = tasksArray.map(function (task) {
+    return task.target.project;
+});
 var sliceSize = Math.floor(projectsArray.length / jobCount);
 projectsArray =
     jobIndex < jobCount
         ? projectsArray.slice(sliceSize * (jobIndex - 1), sliceSize * jobIndex)
         : projectsArray.slice(sliceSize * (jobIndex - 1));
 var projects = projectsArray.join(',');
-var cmd = target !== 'e2e'
-    ? "npx nx run-many --target=" + target + " --projects=" + projects + " --parallel " + restArgs()
-    : "npx nx run-many --target=" + target + " --projects=" + projects + " " + restArgs();
+var cmd =
+    target !== 'e2e'
+        ? 'npx nx run-many --target=' +
+          target +
+          ' --projects=' +
+          projects +
+          ' --parallel ' +
+          restArgs()
+        : 'npx nx run-many --target=' +
+          target +
+          ' --projects=' +
+          projects +
+          ' ' +
+          restArgs();
 if (projects.length > 0)
     (0, child_process_1.execSync)(cmd, { stdio: [0, 1, 2] });
 //# sourceMappingURL=run-many.js.map
--- tools/scripts/run-many.ts
+++ tools/scripts/run-many.ts
@@ -6,15 +6,15 @@ const jobCount = Number(process.argv[4]);
 const isMain = process.argv[5] === 'main';
 const baseSha = isMain ? 'main~1' : 'main';
 
-interface Task {
+interface ITask {
     id: string;
     overrides: {};
-    target: Target;
+    target: ITarget;
     command: string;
     outputs: [];
 }
 
-interface Target {
+interface ITarget {
     project: string;
     target: string;
 }
@@ -29,9 +29,9 @@ function restArgs() {
 let affected = `npx nx print-affected --base=${baseSha} --target=${target}`;
 affected = execSync(affected).toString('utf-8');
 
-const tasksArray: Task[] = JSON.parse(affected).tasks.sort();
+const tasksArray: ITask[] = JSON.parse(affected).tasks.sort();
 
-let projectsArray = tasksArray.map(task => task.target.project);
+let projectsArray = tasksArray.map((task) => task.target.project);
 
 const sliceSize = Math.floor(projectsArray.length / jobCount);
 
