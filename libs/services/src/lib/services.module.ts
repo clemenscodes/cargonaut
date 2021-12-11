364819ac9fd3b9bb15fb31c2b105082a2c0f0f1c
Clemens Horn <clemens.horn@mni.thm.de>
2021-12-11 14:56:07 +0100

    add services
    Signed-off-by: Clemens Horn <clemens.horn@mni.thm.de>

--- libs/services/src/lib/services.module.ts
+++ libs/services/src/lib/services.module.ts
@@ -6,10 +6,24 @@ import {
     AngularFireAuth,
     AngularFireAuthModule,
 } from '@angular/fire/compat/auth';
+import { DriverService } from './driver/driver.service';
+import { OfferService } from './offer/offer.service';
+import { RatingService } from './rating/rating.service';
+import { RequestService } from './request/request.service';
+import { VehicleService } from './vehicle/vehicle.service';
 
 @NgModule({
     imports: [CommonModule, AngularFireAuthModule],
-    providers: [AlertService, AuthService, AngularFireAuth],
+    providers: [
+        AlertService,
+        AuthService,
+        AngularFireAuth,
+        DriverService,
+        OfferService,
+        RatingService,
+        RequestService,
+        VehicleService,
+    ],
     exports: [AngularFireAuthModule],
 })
 export class ServicesModule {}
