b0f7a863c3d5c59ee4e890b038b1b668abaf725c
Marco Diaz Garcia <marco.diaz.garcia@mni.thm.de>
2021-12-15 13:53:46 +0100

    updated dashboard

--- libs/components/add-offer-modal/src/lib/add-offer-modal.module.ts
+++ libs/components/add-offer-modal/src/lib/add-offer-modal.module.ts
@@ -1,12 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { AddOfferModalComponent } from './add-offer-modal/add-offer-modal.component';
+import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
 import { FormsModule } from '@angular/forms';
 
 @NgModule({
     imports: [
         CommonModule,
         FormsModule,
+        NgbModule
     ],
     declarations: [
       AddOfferModalComponent
