651316df421a35fc825576dd218e3df910c9cffc
Marco Diaz Garcia <marco.diaz.garcia@mni.thm.de>
2021-12-15 10:23:03 +0100

    dashboard html and css done

--- libs/components/dashboard/src/lib/dashboard.module.ts
+++ libs/components/dashboard/src/lib/dashboard.module.ts
@@ -2,9 +2,15 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { DashboardRoutingModule } from './dashboard/dashboard-routing.module';
+import { MatTabsModule } from '@angular/material/tabs';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 
 @NgModule({
-    imports: [CommonModule, DashboardRoutingModule],
+    imports: [CommonModule,
+        DashboardRoutingModule,
+        MatTabsModule,
+        FontAwesomeModule
+    ],
     declarations: [DashboardComponent],
     exports: [DashboardComponent],
 })
