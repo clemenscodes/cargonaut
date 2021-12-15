db6e13d4d7e02cca596844fedeeefccbbf23a9a8
Clemens Horn <clemens.horn@mni.thm.de>
2021-12-15 19:39:37 +0100

    display uploaded photo works
    Signed-off-by: Clemens Horn <clemens.horn@mni.thm.de>

--- libs/components/upload/src/lib/upload/upload.component.ts
+++ libs/components/upload/src/lib/upload/upload.component.ts
@@ -4,9 +4,9 @@ import {
     AngularFireUploadTask,
 } from '@angular/fire/compat/storage';
 // import { UploadService } from '@services';
-import { Observable } from 'rxjs';
+import { Observable, tap } from 'rxjs';
 import firebase from 'firebase/compat/app';
-// import { finalize, Observable } from 'rxjs';
+import { UploadService } from '@services';
 
 @Component({
     selector: 'cargonaut-upload',
@@ -14,18 +14,5 @@ import firebase from 'firebase/compat/app';
     styleUrls: ['./upload.component.scss'],
 })
 export class UploadComponent {
-    constructor(private storage: AngularFireStorage) {}
-    task!: AngularFireUploadTask;
-    snapshot!: Observable<firebase.storage.UploadTaskSnapshot | undefined>;
-    // eslint-disable-next-line @typescript-eslint/no-explicit-any
-    startUpload(event: any) {
-        const file = event.target.files[0];
-        if (file?.type.split('/')[0] !== 'image') {
-            console.error('Man kann nur Bilder hochladen.');
-            return;
-        }
-        const path = `${new Date().getTime()}_${file.name}`;
-        this.task = this.storage.ref(path).put(file);
-        this.snapshot = this.task.snapshotChanges();
-    }
+    constructor(public uploadService: UploadService) {}
 }
--- libs/services/src/lib/upload/upload.service.ts
+++ libs/services/src/lib/upload/upload.service.ts
@@ -1,12 +1,55 @@
 import { Injectable } from '@angular/core';
-import { AngularFireStorage } from '@angular/fire/compat/storage';
-// import { AngularFirestore } from '@angular/fire/compat/firestore';
-// import { Upload } from '@api-interfaces';
-// import firebase from 'firebase/compat/app';
+import { AngularFirestore } from '@angular/fire/compat/firestore';
+import {
+    AngularFireStorage,
+    AngularFireUploadTask,
+} from '@angular/fire/compat/storage';
+import { UploadTaskSnapshot } from '@angular/fire/compat/storage/interfaces';
+import { Observable, tap } from 'rxjs';
 
 @Injectable({
     providedIn: 'root',
 })
 export class UploadService {
-    constructor(private storage: AngularFireStorage) {}
+    private basePath = '/images';
+    file!: File;
+    url = '';
+    constructor(
+        private storage: AngularFireStorage,
+        private afs: AngularFirestore
+    ) {}
+    task!: AngularFireUploadTask;
+    snapshot!: Observable<UploadTaskSnapshot | undefined>;
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    async startUpload(event: any) {
+        this.file = event.target.files[0];
+        if (this.file) {
+            if (this.file.type.split('/')[0] !== 'image') {
+                console.error('Man kann nur Bilder hochladen.');
+                return;
+            }
+            const path = `${this.basePath}/${new Date().getTime()}_${
+                this.file.name
+            }`;
+            this.task = this.storage.ref(path).put(this.file);
+            this.snapshot = this.task.snapshotChanges().pipe(
+                tap((snap) => {
+                    if (snap?.bytesTransferred === snap?.totalBytes) {
+                        if (snap) {
+                            this.afs
+                                .collection('photos')
+                                .add({ path, size: snap?.totalBytes });
+                            this.getUrl(snap);
+                        }
+                    }
+                })
+            );
+        }
+    }
+
+    private async getUrl(snap: UploadTaskSnapshot) {
+        const url = await snap.ref.getDownloadURL();
+        this.url = url; //store the URL
+        console.log(this.url);
+    }
 }
