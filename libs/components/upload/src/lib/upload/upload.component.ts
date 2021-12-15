11c91d12e8fc1c191955c472ed205616716c3bdb
Clemens Horn <clemens.horn@mni.thm.de>
2021-12-15 19:10:51 +0100

    upload works
    Signed-off-by: Clemens Horn <clemens.horn@mni.thm.de>

--- libs/components/upload/src/lib/upload/upload.component.ts
+++ libs/components/upload/src/lib/upload/upload.component.ts
@@ -16,14 +16,7 @@ import firebase from 'firebase/compat/app';
 export class UploadComponent {
     constructor(private storage: AngularFireStorage) {}
     task!: AngularFireUploadTask;
-    percentage!: Observable<number | undefined>;
     snapshot!: Observable<firebase.storage.UploadTaskSnapshot | undefined>;
-    downloadURL!: Promise<string>;
-    isHovering!: boolean;
-    toggleHover(event: boolean) {
-        this.isHovering = event;
-    }
-
     // eslint-disable-next-line @typescript-eslint/no-explicit-any
     startUpload(event: any) {
         const file = event.target.files[0];
@@ -31,28 +24,8 @@ export class UploadComponent {
             console.error('Man kann nur Bilder hochladen.');
             return;
         }
-        const path = `test/${new Date().getTime()}_${file.name}`;
+        const path = `${new Date().getTime()}_${file.name}`;
         this.task = this.storage.ref(path).put(file);
-        this.percentage = this.task.percentageChanges();
         this.snapshot = this.task.snapshotChanges();
-        this.downloadURL = this.task.task.snapshot.ref.getDownloadURL();
     }
-
-    isActive(snapshot: firebase.storage.UploadTaskSnapshot) {
-        return (
-            snapshot.state === 'running' &&
-            snapshot.bytesTransferred < snapshot.totalBytes
-        );
-    }
-
-    //     const fileRef = this.storage.ref(filePath);
-    //     const task = this.storage.upload(filePath, file);
-
-    //     // observe percentage changes
-    //     this.uploadPercent = task.percentageChanges();
-    //     // get notified when the download URL is available
-    //     task.snapshotChanges()
-    //         .pipe(finalize(() => (this.downloadURL = fileRef.getDownloadURL())))
-    //         .subscribe();
-    // }
 }
