6f6259b2fc302e10dbc0f1f15d25bd05593a567c
Leisterbecker <f.winnebald@t-online.de>
2021-12-29 18:51:48 +0100

    implemented vehicle.component.ts

--- libs/components/add-vehicle-modal/src/lib/add-vehicle-modal/add-vehicle-modal.component.ts
+++ libs/components/add-vehicle-modal/src/lib/add-vehicle-modal/add-vehicle-modal.component.ts
@@ -1,15 +1,64 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { NgbActiveModal } from '@ng-bootstrap/ng-bootstrap';
+import { AuthService } from "@services";
+import {
+    AbstractControl,
+    FormBuilder,
+    FormControl,
+    FormGroup,
+    Validators,
+} from "@angular/forms";
 
 @Component({
   selector: 'cargonaut-add-vehicle-modal',
   templateUrl: './add-vehicle-modal.component.html',
   styleUrls: ['./add-vehicle-modal.component.scss']
 })
-export class AddVehicleModalComponent implements OnInit {
+export class AddVehicleModalComponent {
+    vehicleForm: FormGroup;
+    vehicle!: Vehicle;
+    constructor(
+        public activeModal: NgbActiveModal,
+        private authService: AuthService,
+        private fb: FormBuilder
+    ) {
+        this.vehicleForm = this.fb.group({
+            marke: new FormControl("", [Validators.required]),
+            art: new FormControl("", [Validators.required]),
+            baujahr: new FormControl(0, [Validators.required]),
+            sitze: new FormControl(0, [Validators.required]),
+            stauraum: new FormControl(0, [Validators.required]),
+        });
+    }
+    get marke(): AbstractControl {
+        return this.vehicleForm.controls.marke;
+    }
 
-  constructor() { }
+    get art(): AbstractControl {
+        return this.vehicleForm.controls.art;
+    }
 
-  ngOnInit(): void {
-  }
+    get baujahr(): AbstractControl {
+        return this.vehicleForm.controls.baujahr;
+    }
+
+    get sitze(): AbstractControl {
+        return this.vehicleForm.controls.sitze;
+    }
+
+    get stauraum(): AbstractControl {
+        return this.vehicleForm.controls.stauraum;
+    }
+
+    save(): void {
+        const { uid } = this.authService.getCurrentUser();
+        this.vehicle = {
+            //todo vehicle
+        }
+        console.log(this.vehicle);
+        this.activeModal.close(this.vehicle);
+    }
+
+ 
 
 }
