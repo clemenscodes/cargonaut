280704000840643684f2fa4320a5bb7d58abc6c1
Leisterbecker <f.winnebald@t-online.de>
2022-01-15 14:47:39 +0100

    added libs of request, request-item and related modal for request creation

---
+++ libs/components/add-request-modal/src/lib/add-request-modal/add-request-modal.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'cargonaut-add-request-modal',
+  templateUrl: './add-request-modal.component.html',
+  styleUrls: ['./add-request-modal.component.scss']
+})
+export class AddRequestModalComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
---
+++ libs/components/request-item/README.md
@@ -0,0 +1,7 @@
+# request-item
+
+This library was generated with [Nx](https://nx.dev).
+
+## Running unit tests
+
+Run `nx test request-item` to execute the unit tests.
