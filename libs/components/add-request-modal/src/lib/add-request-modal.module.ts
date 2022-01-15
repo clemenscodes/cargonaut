1a5089a18133132f730fea43ee00642abe0e3921
Leisterbecker <f.winnebald@t-online.de>
2022-01-15 14:47:39 +0100

    added libs of request, request-item and related modal for request creation

---
+++ libs/components/add-request-modal/src/lib/add-request-modal.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from "@angular/core";
+import { CommonModule } from "@angular/common";
+import { AddRequestModalComponent } from './add-request-modal/add-request-modal.component';
+
+@NgModule({
+    imports: [CommonModule],
+    declarations: [
+      AddRequestModalComponent
+    ],
+})
+export class AddRequestModalModule {}
---
+++ libs/components/request-item/src/lib/request-item.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from "@angular/core";
+import { CommonModule } from "@angular/common";
+import { RequestItemComponent } from './request-item/request-item.component';
+
+@NgModule({
+    imports: [CommonModule],
+    declarations: [
+      RequestItemComponent
+    ],
+})
+export class RequestItemModule {}
