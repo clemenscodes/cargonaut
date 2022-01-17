55bb79946ed91eb35d79839ad450a0fb0c2ff352
Leisterbecker <f.winnebald@t-online.de>
2022-01-17 08:12:46 +0100

    users now see only own offers in dashboard

--- libs/services/src/lib/offer/offer.service.ts
+++ libs/services/src/lib/offer/offer.service.ts
@@ -4,7 +4,9 @@ import {
     AngularFirestoreCollection,
 } from "@angular/fire/compat/firestore";
 import { Offer } from "@api-interfaces";
-import { map, Observable } from "rxjs";
+import { userInfo } from "os";
+import { map, filter, tap, Observable } from "rxjs";
+import { AuthService } from "../auth/auth.service";
 @Injectable({
     providedIn: "root",
 })
@@ -12,7 +14,8 @@ export class OfferService {
     offersCollection: AngularFirestoreCollection<Offer> =
         this.afs.collection<Offer>("offers", (ref) => ref.limit(5));
     offers: Observable<Offer[]>;
-    constructor(public afs: AngularFirestore) {
+    offersByUser: Observable<Offer[]>;
+    constructor(public afs: AngularFirestore, public auth: AuthService) {
         this.offers = this.offersCollection.snapshotChanges().pipe(
             map((actions) =>
                 actions.map((a) => {
@@ -21,6 +24,17 @@ export class OfferService {
                     return { id, ...data };
                 })
             )
+        );
+
+        this.offersByUser = this.offersCollection.snapshotChanges().pipe(
+            map((elements) => 
+                elements.filter((el) => el.payload.doc.data().userId === this.auth.getCurrentUser().uid)
+                        .map((el) => {
+                            const data = el.payload.doc.data() as Offer;
+                            const id = el.payload.doc.id;
+                            return { id, ...data };
+                        }) 
+                )
         );
     }
     addOffer(offer: Offer) {
@@ -37,4 +51,5 @@ export class OfferService {
             });
         });
     }
+
 }
