56bfbf619d407b7fcc0be4e196771a51a83a6225
Marco Diaz Garcia <marco.diaz.garcia@mni.thm.de>
2022-01-18 10:15:18 +0100

    offer status funktionality fully implemented

--- libs/components/dashboard-inactive-offer/src/lib/dashboard-inactive-offer.module.ts
+++ libs/components/dashboard-inactive-offer/src/lib/dashboard-inactive-offer.module.ts
@@ -1,9 +1,15 @@
 import { CUSTOM_ELEMENTS_SCHEMA, NgModule } from "@angular/core";
 import { CommonModule } from "@angular/common";
 import { DashboardInactiveOfferComponent } from './dashboard-inactive-offer/dashboard-inactive-offer.component';
+import { MatDialogModule } from "@angular/material/dialog";
+import { StatusModalModule } from "@cargonaut/status-modal";
 
 @NgModule({
-    imports: [CommonModule],
+    imports: [
+      CommonModule,
+      MatDialogModule,
+      StatusModalModule
+    ],
     declarations: [
       DashboardInactiveOfferComponent
     ],
